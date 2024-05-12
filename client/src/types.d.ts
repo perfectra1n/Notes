33d53fa9b2ff6310632470e5b3500f95c4e5b462
Elian Doran <contact@eliandoran.me>
2024-05-12 22:09:26 +0300

    #156: Improve global type declarations

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,7 +1,42 @@
-declare interface Window {
-    logError(message: string);
-    logInfo(message: string);
+import FNote from "./entities/fnote";
+
+interface ElectronProcess {
+    type: string;
 }
 
-declare var logError: (message: string) => void;
-declare var logInfo: (message: string) => void;
+interface CustomGlobals {
+    isDesktop: boolean;
+    isMobile: boolean;
+    device: "mobile" | "desktop";
+    getComponentsByEl: (el: unknown) => unknown;
+    getHeaders: Promise<Record<string, string>>;
+    getReferenceLinkTitle: (href: string) => Promise<string>;
+    getReferenceLinkTitleSync: (href: string) => string;
+    getActiveContextNote: FNote;
+    requireLibrary: (library: string) => Promise<void>;
+    ESLINT: { js: string[]; };
+    appContext: AppContext;
+    froca: Froca;
+    treeCache: Froca;
+    importMarkdownInline: () => Promise<unknown>;
+    SEARCH_HELP_TEXT: string;
+    activeDialog: JQuery<HTMLElement> | null;
+}
+
+declare global {
+    interface Window {
+        logError(message: string);
+        logInfo(message: string);
+    
+        process?: ElectronProcess;
+        glob?: CustomGlobals;
+    }
+
+    interface JQuery {
+        autocomplete: (action: "close") => void;
+    }
+
+    declare var logError: (message: string) => void;
+    declare var logInfo: (message: string) => void;
+    declare var glob: CustomGlobals;
+}
