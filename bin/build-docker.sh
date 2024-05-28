b65f8ddf78ac06fc22cba1e7b4465574f6f7a6cc
alexpietsch <54153428+alexpietsch@users.noreply.github.com>
2024-05-28 19:41:51 +0200

    enable docker build

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-VERSION=`jq -r ".version" package.json`
+VERSION='develop'
 SERIES=${VERSION:0:4}-latest
 
 cat package.json | grep -v electron > server-package.json
 
-sudo docker build -t zadam/trilium:$VERSION --network host -t zadam/trilium:$SERIES .
+sudo docker build -t notes:$VERSION --network host -t notes:$SERIES .
 
 if [[ $VERSION != *"beta"* ]]; then
-  sudo docker tag zadam/trilium:$VERSION zadam/trilium:latest
+  sudo docker tag notes:$VERSION notes:latest
 fi
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
-const path = require('path');
-const assetPath = require('./src/services/asset_path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        setup: './src/public/app/setup.ts',
-        mobile: './src/public/app/mobile.js',
-        desktop: './src/public/app/desktop.js',
-    },
-    output: {
-        publicPath: `${assetPath}/app-dist/`,
-        path: path.resolve(__dirname, 'src/public/app-dist'),
-        filename: '[name].js',
-    },
-    devtool: 'source-map',
-    target: 'electron-renderer',
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  entry: {
+    setup: "./src/public/app/setup.js",
+    mobile: "./src/public/app/mobile.js",
+    desktop: "./src/public/app/desktop.js",
+  },
+  output: {
+    publicPath: `assets/v0.63.5/app-dist/`,
+    path: path.resolve(__dirname, "src/public/app-dist"),
+    filename: "[name].js",
+  },
+  devtool: "source-map",
+  target: "electron-renderer",
 };
